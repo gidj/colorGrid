ee0c38ad60d18389989ae9be0e1ab7a2ed457931
Gideon VanRiette <gvanriette@gmail.com>
2014-05-08 14:34:03 -0500

    Added default view

--- colorGrid/urls.py
+++ colorGrid/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'colorGrid.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'colorGrid.views.default'),
     url(r'^admin/', include(admin.site.urls)),
 )
