58a2c4294a55e65ab3ae3efaf84cc6fbadbfdf0f
Gideon VanRiette <gvanriette@gmail.com>
2014-05-08 17:12:13 -0500

    Basic table structure using templates

--- colorGrid/views.py
+++ colorGrid/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 def default(request):
-    return render(request, 'index.html')
+    grid = []
+    for row in xrange(1, 10, 3):
+        grid.append(range(1*row, (row+3)))
+
+    context = {'grid': grid,
+               'title': 'Grid',}
+    return render(request, 'index.html', context)
 
