00d33f19e43151a28e9627c729bcc6d24237f9a2
Gideon VanRiette <gvanriette@gmail.com>
2014-05-08 16:00:39 -0500

    Get base and index templates connected to the view

--- colorGrid/settings.py
+++ colorGrid/settings.py
@@ -10,8 +10,19 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from unipath import Path
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PROJECT_DIR = Path(__file__).ancestor(2)
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child('templates'),
+)
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('assets'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
--- colorGrid/views.py
+++ colorGrid/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 def default(request):
-    pass
+    return render(request, 'index.html')
 
