002dfb5c2fc30c35c2009e623cbd4018fc7acb13
Gideon VanRiette <gvanriette@gmail.com>
2014-05-09 17:33:14 -0500

    change models to forms.py

--- colorGrid/views.py
+++ colorGrid/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from utils import random_octal_str
+import colorGrid.models.GridForm
 
 def index(request):
+    if request.method == "POST":
+        form = GridForm(request.POST)
+        if form.is_valid():
+            pass
+
     return render(request, 'index.html')
 
 def grid(request):
