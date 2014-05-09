3f6a3ef6e8fe2b1fe9962d2bfe00f52e23b2e638
Gideon VanRiette <gvanriette@gmail.com>
2014-05-08 23:09:08 -0500

    added array that pulls in from template, as well as separate javascript

---
+++ assets/js/grid.js
@@ -0,0 +1,13 @@
+function oct2hex_from_string(str) {
+
+
+}
+
+function print_arr(arr) {
+    body = document.body;
+    para = document.createElement('p');
+    para.appendChild(document.createTextNode(arr));
+    body.appendChild(para);
+}
+
+
